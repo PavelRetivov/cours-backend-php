20821d88b0fdcfd612b67511b4acae4bd26c3988
Pavel Retivov <wertualyti@gmail.com>
2025-04-06 18:44:34 +0300

    update logic

--- task5.php
+++ task5.php
@@ -5,37 +5,39 @@ function isUriValid($uri){
     return (bool)preg_match($regex, $uri);
 }
 
+function responseFile($domain, $file)
+{
+    try {
+        echo file_get_contents($domain . $file);
+    }catch (Exception $e){
+        echo json_encode(["status" => "404 Not Found", "message" => "file not found"]);
+        return;
+    }
+}
+
 if($_SERVER['REQUEST_METHOD'] == "GET"){
     $requestString = file_get_contents("php://input");
     $request = json_decode($requestString, true);
     $uri = isUriValid($request['uri']) ? $request['uri'] : null;
+    $serverDomain = ["another.shpp.me" => 'another/', "student" => 'student/'];
 
-    if($uri){
-        [$folder, $file ] = explode("/", trim($uri, "/"), 2);
-        if(in_array($folder, ["another.shpp.me", "student"])){
-            if($folder === "another.shpp.me"){
-                try {
-                    echo file_get_contents("another/" . $file);
-                }catch (Exception $e){
-                    echo json_encode(["status" => "404 Not Found", "message" => "file not found"]);
-                    return;
-                }
-            }
-            if($folder === "student"){
-                try {
-                    echo file_get_contents("student/" . $file);
-                }catch (Exception $e){
-                    echo json_encode(["status" => "404 Not Found", "message" => "file not found"]);
-                    return;
-                }
-            }
-        }else{
-            echo json_encode(["status" => "400 Bad Request", "message" => "uri don`t right"]);
-        }
-    }else{
-        echo json_encode(["status" => "400 Bad Request", "message" => "uri don`t right"]);
+    if(!$uri) {
+        echo json_encode(["status" => "400 Bad Request", "message" => "url does not exist"]);
+
+        return;
     }
-}else{
-    echo json_encode(["status" => "400 Bad Request", "message" => "method dont right"]);
-};
 
+    [$requestDomain, $file ] = explode("/", trim($uri, "/"), 2);
+
+    if(!isset($serverDomain[$requestDomain])){
+        echo json_encode(["status" => "400 Bad Request", "message" => "url does not exist"]);
+
+        return;
+    }
+
+    responseFile($serverDomain[$requestDomain], $file);
+
+    return;
+}
+
+echo json_encode(["status" => "400 Bad Request", "message" => "method dont right"]);
